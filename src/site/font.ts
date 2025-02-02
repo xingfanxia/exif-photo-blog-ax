55314a644f9facb30f98c483ecfdb845eb052e4f
Xingfan Xia(AX) <xingfanxia@gmail.com>
2025-02-02 05:10:45 -0800

    fix build issue for chinese text and sort tags by count

--- src/site/font.ts
+++ src/site/font.ts
@@ -3,30 +3,40 @@ import path from 'path';
 import { cwd } from 'process';
 
 const FONT_FAMILY_IBM_PLEX_MONO = 'IBMPlexMono';
+const FONT_FAMILY_NOTO_SANS = 'NotoSansSC';
 
-const getFontData = async () => {
+const getFontData = async (fontPath: string) => {
   let data;
   if (typeof fs !== 'undefined') {
-    data = fs.readFileSync(path.join(
-      cwd(),
-      '/public/fonts/IBMPlexMono-Medium.ttf',
-    ));
+    data = fs.readFileSync(path.join(cwd(), fontPath));
   } else {
-    data = await fetch(new URL(
-      '/public/fonts/IBMPlexMono-Medium.ttf',
-      import.meta.url,
-    )).then(res => res.arrayBuffer());
+    data = await fetch(new URL(fontPath, import.meta.url))
+      .then(res => res.arrayBuffer());
   }
   return data;
 };
 
-export const getIBMPlexMonoMedium = () => getFontData()
-  .then(data => ({
+export const getIBMPlexMonoMedium = async () => {
+  const [latinFont, cjkFont] = await Promise.all([
+    getFontData('/public/fonts/IBMPlexMono-Medium.ttf'),
+    getFontData('/public/fonts/NotoSansSC-Medium.ttf')
+  ]);
+
+  return {
     fontFamily: FONT_FAMILY_IBM_PLEX_MONO,
-    fonts: [{
-      name: FONT_FAMILY_IBM_PLEX_MONO,
-      data,
-      weight: 500,
-      style: 'normal',
-    } as const],
-  }));
+    fonts: [
+      {
+        name: FONT_FAMILY_IBM_PLEX_MONO,
+        data: latinFont,
+        weight: 500,
+        style: 'normal',
+      } as const,
+      {
+        name: FONT_FAMILY_NOTO_SANS,
+        data: cjkFont,
+        weight: 500,
+        style: 'normal',
+      } as const,
+    ],
+  };
+};
